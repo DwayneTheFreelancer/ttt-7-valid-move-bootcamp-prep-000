015ecd064df670565c858d382f420c06dfdcafc8
Dwayne Munro <dwaynemunro1845@gmail.com>
2019-01-31 19:24:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,9 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index) 
-  
+  if(board == position_taken)
+    
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
